44cf9de8a4dcef3bafb4c8948fdea61b6ebd9039
Harshit <Harshitdb3@gmail.com>
2023-07-22 00:43:06 +0530

    Deleted others

--- src/main/java/com/nextory/techtest/controllers/CommentController.java
+++ src/main/java/com/nextory/techtest/controllers/CommentController.java
@@ -1,16 +1,21 @@
 package com.nextory.techtest.controllers;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.nextory.techtest.models.Book;
 import com.nextory.techtest.models.Comment;
 import com.nextory.techtest.services.CommentService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@RestController
 public class CommentController {
 
     @Autowired
@@ -20,16 +25,17 @@ public class CommentController {
     public void submitComment(@RequestParam("bookId") Long bookId,
             @RequestParam("pseudonyme") String pseudonyme,
             @RequestParam("content") String content,
-            HttpServletResponse response) {
+            HttpServletRequest request,
+            HttpServletResponse response)  throws IOException  {
         Book book = new Book();
         book.setId(bookId);
         Comment comment = new Comment();
         comment.setPseudonyme(pseudonyme);
         comment.setContent(content);
-        comment.setBook(new Book());
+        comment.setBook(book);
         comment.setCreatedAt(LocalDateTime.now());
         comment.setUpdatedAt(LocalDateTime.now());
         commentService.addComment(comment);
-        response.setStatus(HttpServletResponse.SC_OK);
+        response.sendRedirect("/book/" + bookId);
     }
 }
